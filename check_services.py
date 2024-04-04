a94b42417d8ccdfef666b7dcf17098f657c16748
Vladimi2boom <97739181+Vladimi2boom@users.noreply.github.com>
2024-04-04 23:05:28 +0300

    Add files via upload

--- check_services.py
+++ check_services.py
@@ -24,10 +24,9 @@ def send_message(message : str):
 		except Exception as e:
 			print(f"error: {e}")
 	if GOTIFY_ON:
-		data = {"message": message.replace("*", "").replace("\t", "")}
-		headers = {"X-Gotify-Key": GOTIFY_TOKEN, "Content-Type": "application/json"}
+		message = message.replace("*", "").replace("\t", "")
 		try:
-			response = requests.post(GOTIFY_WEB, json=data, headers=headers)
+			response = requests.post(f'{GOTIFY_WEB}?token={GOTIFY_TOKEN}', json={"message": message})
 		except HTTPError as e:
 			print(f"reason: {e.reason}")
 
