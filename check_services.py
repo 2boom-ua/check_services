27ee742497e0d05f85c292d8aa93f0e824db8f2a
Vladimi2boom <97739181+Vladimi2boom@users.noreply.github.com>
2024-04-09 19:01:57 +0300

    Add files via upload

--- check_services.py
+++ check_services.py
@@ -11,6 +11,7 @@ import requests
 from gotify import Gotify
 import discord_notify as dn
 from schedule import every, repeat, run_pending
+from pushbullet import Pushbullet
 
 def send_message(message : str):
 	message = message.replace("\t", "")
@@ -38,6 +39,11 @@ def send_message(message : str):
 			requests.post(f"{NTFY_WEB}/{NTFY_SUB}", data=message.encode(encoding='utf-8'), headers={"Title": header})
 		except Exception as e:
 			print(f"error: {e}")
+	if PUSHBULLET_ON:
+		try:
+			push = pb.push_note(header, message)
+		except Exception as e:
+			print(f"error: {e}")
 			
 def messaging_service():
 	messaging = ""
@@ -49,6 +55,8 @@ def messaging_service():
 		messaging += "- messenging: Gotify,\n"
 	if NTFY_ON:
 		messaging += "- messenging: Ntfy,\n"
+	if PUSHBULLET_ON:
+		messaging += "- messenging: Pushbullet,\n"
 	return messaging
 
 if __name__ == "__main__":	
@@ -66,6 +74,7 @@ if __name__ == "__main__":
 		DISCORD_ON = parsed_json["DISCORD"]["ON"]
 		GOTIFY_ON = parsed_json["GOTIFY"]["ON"]
 		NTFY_ON = parsed_json["NTFY"]["ON"]
+		PUSHBULLET_ON = parsed_json["PUSHBULLET"]["ON"]
 		if TELEGRAM_ON:
 			TOKEN = parsed_json["TELEGRAM"]["TOKEN"]
 			CHAT_ID = parsed_json["TELEGRAM"]["CHAT_ID"]
@@ -79,6 +88,9 @@ if __name__ == "__main__":
 		if NTFY_ON:
 			NTFY_WEB = parsed_json["NTFY"]["WEB"]
 			NTFY_SUB = parsed_json["NTFY"]["SUB"]
+		if PUSHBULLET_ON:
+			PUSHBULLET_API = parsed_json["PUSHBULLET"]["API"]
+			pb = Pushbullet(PUSHBULLET_API)
 		MIN_REPEAT = int(parsed_json["MIN_REPEAT"])
 		send_message(f"*{HOSTNAME}* (services)\nservices monitor:\n{messaging_service()}- polling period: {MIN_REPEAT} minute(s).")
 	else:
