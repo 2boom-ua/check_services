17a19c052a88ed70dd5e11cd1c97953e0c46df05
2boom <97739181+2boom-ua@users.noreply.github.com>
2024-09-22 20:11:26 +0300

    Update check_services.py

--- check_services.py
+++ check_services.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-"""Copyright (c) 2024 2boom. This project is licensed under the MIT License - see the [MIT License](https://opensource.org/licenses/MIT) for details."""
+"""Copyright (c) 2024 2boom. This project is licensed under the MIT License - see the https://opensource.org/licenses/MIT for details."""
 
 import json
 import os.path
